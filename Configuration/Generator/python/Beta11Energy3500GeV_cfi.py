2a20f3a497bef2cbe8442e2170e8de95fa14c2de
ziaber <marcinziaber8@gmail.com>
2016-08-02 14:17:08 +0200

    copied totem proton reconstruction module from 7.5 (cherry picked from commit 5fc8afd) (cherry picked from commit 9336664)
    migrated Xerces to version 3.1
    (cherry picked from commit c7fcdf3)
    (cherry picked from commit b64bec0)
    
    migrated ROOT to version 6
    (cherry picked from commit 6c83dee)
    (cherry picked from commit 211d5aa)
    
    moved transport model, pshysics and setup
    (cherry picked from commit 95a1d3c)
    (cherry picked from commit 5ad3dd2)
    
    proton gun config
    (cherry picked from commit f47f962)
    
    proton gun config energy and eta change
    (cherry picked from commit 40c03b2)
    
    merging ProtonTransportFunctions
    (cherry picked from commit ede5840)
    
    TotemCondFormats
    (cherry picked from commit 8a0e6ee)
    
    ProtonTransportRcd imports fix
    (cherry picked from commit 35da900)
    
    ProtonTransportRcd fix
    (cherry picked from commit b960726)
    
    TotemCondFormats
    (cherry picked from commit b2a80b1)
    
    removing unused files
    (cherry picked from commit 4d49cec)
    
    work in progress
    (cherry picked from commit 9b95068)
    
    work in progress
    (cherry picked from commit 15bf721)
    
    work in progress
    (cherry picked from commit f2edc5a)
    
    work in progress
    (cherry picked from commit aaa2ca5)
    
    work in progress
    (cherry picked from commit 97067ed)
    
    work in progress, config changes
    (cherry picked from commit 52029fb)
    
    changed proton gun config
    (cherry picked from commit 1096d6e)
    
    work on config in progress
    (cherry picked from commit 6742a4a)
    
    aaaa
    (cherry picked from commit b0d895c)
    
    aaa
    (cherry picked from commit 5e7377d)
    
    aaaa
    (cherry picked from commit 4b20e2f)
    
    abc
    (cherry picked from commit 1c88d03)
    
    work on config in progress
    (cherry picked from commit 8b8e52b)
    
    work on config in progress
    (cherry picked from commit 17ac46b)
    
    work on config in progress
    (cherry picked from commit fbee248)
    
    work on config in progress
    (cherry picked from commit 5d1f2fb)
    
    work on config in progress
    (cherry picked from commit 88b9c12)
    
    RunManager changes
    (cherry picked from commit 2b1d34a)
    
    RunManager changes
    (cherry picked from commit 8b862e6)
    
    parametrization
    (cherry picked from commit 47b139a)
    
    StackingAction change
    (cherry picked from commit 24ebebb)
    
    migration of totem gun
    (cherry picked from commit b8fe584)
    
    import fix in gun
    (cherry picked from commit 03b1699)
    
    import fix in gun
    (cherry picked from commit 9a7ba43)
    
    migrating gun
    (cherry picked from commit 577766e)
    
    migrating gun work in progress
    (cherry picked from commit b216d2b)
    
    migrating gun work in progress
    (cherry picked from commit 41f695d)
    
    config
    (cherry picked from commit 2f99908)
    
    config
    (cherry picked from commit 03a2c44)
    
    config
    (cherry picked from commit cdcb27e)

---
+++ Configuration/Generator/python/Beta11Energy3500GeV_cfi.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDProducer("FlatProtonLogKsiLogTGun",
+    LogKsiDistShape = cms.untracked.bool(True),
+    LogTDistShape = cms.untracked.bool(True),
+    GenerateMirroredProtons = cms.untracked.bool(False),
+
+    RightArm = cms.untracked.bool(True),
+    LeftArm = cms.untracked.bool(True),
+    NominalEnergy = cms.untracked.double(3500.0),
+    MinT = cms.untracked.double(-0.0001),
+    MaxT = cms.untracked.double(-0.3),
+    MinPhi = cms.untracked.double(-3.141592654),
+    MaxPhi = cms.untracked.double(3.141592654),
+    MinKsi = cms.untracked.double(-0.0001),
+    MaxKsi = cms.untracked.double(-0.32),
+    Verbosity = cms.untracked.int32(0),
+    MaximumIterationNumber = cms.untracked.int32(20), 
+    instanceLabel = cms.untracked.string('generator')
+)
+
+
