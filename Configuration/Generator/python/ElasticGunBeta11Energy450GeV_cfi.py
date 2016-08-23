b8fe58483182c863c3d75c8971026d091650e733
ziaber <marcinziaber8@gmail.com>
2016-08-23 11:24:11 +0200

    migration of totem gun

---
+++ Configuration/Generator/python/ElasticGunBeta11Energy450GeV_cfi.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDProducer("FlatProtonLogKsiLogTGun",
+    LogKsiDistShape = cms.untracked.bool(True),
+    LogTDistShape = cms.untracked.bool(True),
+    GenerateMirroredProtons = cms.untracked.bool(True),
+
+    RightArm = cms.untracked.bool(True),
+    LeftArm = cms.untracked.bool(True),
+    NominalEnergy = cms.untracked.double(450.0),
+    MinT = cms.untracked.double(-0.005),
+    MaxT = cms.untracked.double(-10),
+    MinPhi = cms.untracked.double(-3.141592654),
+    MaxPhi = cms.untracked.double(3.141592654),
+    MinKsi = cms.untracked.double(-0.0000000001),
+    MaxKsi = cms.untracked.double(-0.00000000009),
+    Verbosity = cms.untracked.int32(0),
+    MaximumIterationNumber = cms.untracked.int32(20), 
+    instanceLabel = cms.untracked.string('generator')
+)
+
+
