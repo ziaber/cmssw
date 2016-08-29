52fe9af901b9b22574cc4d25870ea4c126e405eb
ziaber <marcinziaber8@gmail.com>
2016-08-29 10:33:07 +0200

    detid debugging

--- Geometry/VeryForwardGeometryBuilder/src/DDDTotemRPConstruction.cc
+++ Geometry/VeryForwardGeometryBuilder/src/DDDTotemRPConstruction.cc
@@ -72,8 +72,8 @@ void DDDTotemRPContruction::buildDetGeomDesc(DDFilteredView *fv, DetGeomDesc *gd
 			unsigned int station = (A % 100) / 10;
 			unsigned int rp = A % 10;
 			unsigned int detector = cN[cN.size() - 1];
-      //.std::cout<<"arm:"<<arm<<", station:"<<station<<", rp:"<<rp<<", detector:"<<detector<<std::endl;
-      //.std::cout<<"TotemRPDetId(arm, station, rp, detector) "<<TotemRPDetId(arm, station, rp, detector).DetectorDecId()<<", "<<TotemRPDetId(arm, station, rp, detector).rawId()<<std::endl;
+      std::cout<<"arm:"<<arm<<", station:"<<station<<", rp:"<<rp<<", detector:"<<detector<<std::endl;
+      std::cout<<"TotemRPDetId(arm, station, rp, detector) "<<TotemRPDetId(arm, station, rp, detector).DetectorDecId()<<", "<<TotemRPDetId(arm, station, rp, detector).rawId()<<std::endl;
 			newGD->setGeographicalID(TotemRPDetId(arm, station, rp, detector));
 			//.cout << "A = " << A << "; arm = " << arm << " st = " << station << " rp = " << rp << " det = " << detector << " --> "<< gd->geographicalID().rawId() << endl;
 		}
