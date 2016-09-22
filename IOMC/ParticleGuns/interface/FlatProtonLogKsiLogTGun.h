4cca98e96036d27c224554dbe751d33d88f32ea4
ziaber <marcinziaber8@gmail.com>
2016-09-22 14:54:29 +0200

    work in progress

--- IOMC/ParticleGuns/interface/FlatProtonLogKsiLogTGun.h
+++ IOMC/ParticleGuns/interface/FlatProtonLogKsiLogTGun.h
@@ -1,5 +1,5 @@
-#ifndef IOMC_FlatProtonLogKsiLogTGun_FlatProtonLogKsiLogTGun_h
-#define IOMC_FlatProtonLogKsiLogTGun_FlatProtonLogKsiLogTGun_h
+#ifndef FlatProtonLogKsiLogTGun_h
+#define FlatProtonLogKsiLogTGun_h
 
 #include <string>
 #include "HepPDT/defs.h"
